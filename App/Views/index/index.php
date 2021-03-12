ce7b2ce98f8e377fa3f2104cd1857a38e78a123e
1bertojunior <hjunior854@gmail.com>
2021-03-12 16:04:22 -0300

    new comiit

--- App/Views/index/index.php
+++ App/Views/index/index.php
@@ -6,4 +6,5 @@
 <a href="/login">Login</a>
 <a href="/admin">Admin</a>
 <a href="/agendamento">Agendamento online</a>
-<!-- <a href="/admin">Admin</a> -->
+<a href="/searchagendamento">Consultar agendamento</a>
+<a href="/searchagendamento">Excluir agendamento</a>
